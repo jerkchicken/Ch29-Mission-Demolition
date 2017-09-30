1f6a84e1cbc943ebf03465dc1b94008d96ecd7f1
jerkchicken <markvinegra@gmail.com>
2017-09-29 20:23:11 -0400

    Update 2

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    static public FollowCam S; // a FollowCam Singleton
+                               // fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________________________;
+    // fields set dynamically
+    public GameObject poi; // The point of interest
+    // Interpolate from the current Camera position toward destination
+
+    
+    public float camZ; // The desired Z pos of the camera
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+    void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        if (poi == null) return; // return if there is no poi
+                                 // Get the position of the poi
+        Vector3 destination = poi.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+    }
+}
+
